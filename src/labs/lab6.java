0142444cccf61bb223a625bf36f9ee71d391828e
Karol Belina <karolbelina@gmail.com>
2018-11-21 13:12:06 +0100

    javuh

---
+++ src/labs/lab6.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Main {
+    public static int[] insert(int x, int[] xs) {
+        int n = xs.length;
+        int i = 0;
+        
+        while(i < n && x > xs[i]) {
+            i++;
+        }
+        
+        int[] retVal = new int[n + 1];
+        System.arraycopy(xs, 0, retVal, 0, i);
+        retVal[i] = x;
+        System.arraycopy(xs, i, retVal, i + 1, n - i);
+        
+        return retVal;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(insert(4, new int[]{1, 3, 5, 7})));
+        System.out.println(Arrays.toString(insert(1, new int[]{})));
+        System.out.println(Arrays.toString(insert(100, new int[]{2, 4, 6, 20, 40})));
+    }
+}
