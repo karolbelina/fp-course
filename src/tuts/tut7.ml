bc9c610581fa1adb872dc36d3e4a3d833b901029
Karol Belina <karolbelina@gmail.com>
2018-12-10 19:56:00 +0100

    tut7 finished

--- src/tuts/tut7.ml
+++ src/tuts/tut7.ml
@@ -34,6 +34,11 @@ struct
 	let isEmpty q = q = []
 end;;
 
+(* tests *)
+let q = let open QueueA in enqueue (3, enqueue (2, enqueue (1, empty ())));;
+QueueA.first q;;
+let open QueueA in first (dequeue (dequeue q));;
+
 (* define a module which supports basic operations on a queue represented by a pair or lists *)
 module QueueB : QUEUE_FUN =
 struct
@@ -63,3 +68,56 @@ end;;
 let q = let open QueueB in enqueue (3, enqueue (2, enqueue (1, empty ())));;
 QueueB.first q;;
 let open QueueB in first (dequeue (dequeue q));;
+
+(* signature for the excercise 2 *)
+module type QUEUE_MUT =
+sig
+	type 'a t
+	exception Empty of string
+	exception Full of string
+	val empty: int -> 'a t
+	val enqueue: 'a * 'a t -> unit
+	val dequeue: 'a t -> unit
+	val first: 'a t -> 'a
+	val isEmpty: 'a t -> bool
+	val isFull: 'a t -> bool
+end;;
+
+(* ex 2 *)
+module QueueC : QUEUE_MUT = 
+struct
+	type 'a t = {mutable f : int; mutable r : int; mutable arr : 'a option array}
+	exception Empty of string
+	exception Full of string
+
+	let empty n = {f = 0; r = 0; arr = Array.make (n + 1) None }
+
+	let isEmpty q = q.f = q.r
+
+	let isFull q = (q.r + 1) mod (Array.length q.arr) = q.f
+
+	let enqueue (e, q) =
+		if isFull q then raise (Full "module QueueC: enqueue")
+		else begin
+			q.arr.(q.r) <- Some e;
+			q.r <- (q.r + 1) mod (Array.length q.arr)
+		end
+
+	let dequeue q = if q.f != q.r then q.f <- (q.f + 1) mod (Array.length q.arr)
+
+	let first q = 
+		if isEmpty q then raise (Empty "module QueueC: first")
+		else match q.arr.(q.f) with
+			Some e -> e
+			| None -> failwith "module QueueC: first (implementation error)"
+end;;
+
+(* tests *)
+let q = QueueC.empty 3;;
+QueueC.enqueue (1, q);;
+QueueC.enqueue (2, q);;
+QueueC.enqueue (3, q);;
+QueueC.first q;;
+QueueC.dequeue q;;
+QueueC.dequeue q;;
+QueueC.first q;;
